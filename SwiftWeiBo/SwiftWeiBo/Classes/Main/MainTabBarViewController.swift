bacc3e384267e85f343eb1ce477d76d1a7558e6d
CharType <chartype@163.com>
2016-01-28 15:23:19 +0800

    添加json文件 从json文件中加载控制器名称

--- SwiftWeiBo/SwiftWeiBo/Classes/Main/MainTabBarViewController.swift
+++ SwiftWeiBo/SwiftWeiBo/Classes/Main/MainTabBarViewController.swift
@@ -15,18 +15,51 @@ class MainTabBarViewController: UITabBarController {
         
         tabBar.tintColor = UIColor.orangeColor()
         
+        // 1.获取json文件路径
+         let path = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)
         
-        // 添加子控制器
-        addChildViewController(HomeTableViewController(), title:"首页", imageName: "tabbar_home")
-        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DisCoverTableViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        
+        // 2.通过文件路径创建NSData
+        
+        if let jsonpath = path{
+           let jsonData = NSData(contentsOfFile: jsonpath)
+            
+            do{
+                // 将json转换成Array 
+                // jsonData 是可选类型，必须强制拆包 这句代码容易发生异常，必须try
+              let dictArr =  try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers)
+                
+                // 对数组进行遍历的时候必须指明数组的类型
+                for var dict in dictArr as! [[String:String]]{
+                    addChildViewController(dict["vcName"]!, title: dict["title"]!, imageName: dict["imageName"]!)
+                }
+            }catch{
+            
+             print(error)
+                // 加载文件出现错误  通过代码添加子控制器
+                addChildViewController("HomeTableViewController", title:"首页", imageName: "tabbar_home")
+                addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+                addChildViewController("DisCoverTableViewController", title: "发现", imageName: "tabbar_discover")
+                addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+            }
+        }
+        
+       
         
         
     }
     
-    private func addChildViewController(childController: UIViewController,title:String,imageName:String) {
+    private func addChildViewController(className: String,title:String,imageName:String) {
         
+        // 动态获取命名空间 
+        let namespase = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        
+        // 将字符串转换为类，类名需要加上默认的命名空间
+        let cls :AnyClass? =  NSClassFromString(namespase + "." + className)
+        // 将AnyClass  转换为指定的类型
+        let child = cls as! UIViewController.Type
+        // 创建对象
+        let childController = child.init()
         // 设置childController的一些属性
         childController.tabBarItem.image = UIImage(named: imageName)
         childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
