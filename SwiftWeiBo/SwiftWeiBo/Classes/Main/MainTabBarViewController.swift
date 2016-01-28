ed828e6ce93e0c5e54cc2fc643ef43bfe1376c5b
CharType <chartype@163.com>
2016-01-28 12:48:29 +0800

    添加首页，消息  发现  我  四个模块的导航控制器

--- SwiftWeiBo/SwiftWeiBo/Classes/Main/MainTabBarViewController.swift
+++ SwiftWeiBo/SwiftWeiBo/Classes/Main/MainTabBarViewController.swift
@@ -11,7 +11,35 @@ import UIKit
 class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
+
+        
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        
+        // 添加子控制器
+        addChildViewController(HomeTableViewController(), title:"首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(DisCoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        
         
     }
+    
+    private func addChildViewController(childController: UIViewController,title:String,imageName:String) {
+        
+        // 设置childController的一些属性
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        childController.title = title
+        
+        // 创建导航控制器
+        let nav = UINavigationController()
+        // 将当前控制器包装一个导航控制器
+        nav.addChildViewController(childController)
+        
+        // 将导航控制器添加到当前的子控制器上
+        addChildViewController(nav)
+    }
+    
     
 }
